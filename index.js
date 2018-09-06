9c6f3ed170ff711610e66396c3bb7c1682cf8207
Linda Chang <lindachang186@gmail.com>
2018-09-06 15:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   for(var i = 0; i < katzDeliLine.length; i++) {
     katzDeliLine.push(name);
-    return(`Welcome, ${name}. You are number ${i} in line.`)
+    return(`Welcome, ${name}. You are number ${i+1} in line.`)
   }
 }
