28ce1fa4b8060491337bbbdc267db8c33d0c7797
Linda Chang <lindachang186@gmail.com>
2018-09-06 15:07:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function takeANumber(katzDeliLine, name) {
   var i = katzDeliLine.length;
   return(`Welcome, ${name}. You are number ${i} in line.`)
 }
+
+function takeANumber(katzDeliLine, name){
+  if(i = 0; i < katzDeliLine.length; i++) {
+    katzDeliLine.push(name);
+    return(`Welcome, ${name}. You are number ${i+1} in line.`)
+  }
+}
